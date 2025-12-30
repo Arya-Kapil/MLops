02075976072b949b1345700c160107f87dd05e0b
Kapil Arya <kapil.kapilarya12@gmail.com>
2025-12-30 14:47:26 +0530

    6 commit

---
+++ helper.py
@@ -0,0 +1,11 @@
+def add_two_numbers(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+
+# Example usage
+result = add_two_numbers(5, 3)
+print(result)  # Output: 8
+a ="hleper 2 file"
+print(a)
+b = "1-2-3-6"
